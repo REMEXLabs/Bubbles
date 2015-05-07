431b4555ce0bfb9f17d47ec4cf271496da4a5f3a
Fion Noir <fion.noir@dadracodesign.degit credential-osxkeychaingit config --global user.email fion.noir@dadracodesign.de>
2015-05-07 12:21:27 +0200

    2015-05-07 Demo

---
+++ scripts/bubblesBasicFunctions.js
@@ -0,0 +1,22 @@
+/**
+ * @author Patrick Münster
+ *
+ * Basic functions of Bubbles
+ */
+
+
+$(function () {
+    
+    // Add Stack Overflow project
+    $("#btn_addStackOverflow").click(function() {
+       console.log("add a new project");
+       //open Editor / create Editor
+       $("#acc_projects-stackoverflow").append("<h3>blabla</h3><div>Description and link of the project</div>");
+    });
+    
+    
+    
+
+    
+    
+});
