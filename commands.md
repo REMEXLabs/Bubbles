11cc0260152d4319ce9c9daf7afcc32630f7a6ba
nok <darius.morawiec@nok.onl>
2016-07-12 16:59:42 +0200

    init scaffold

---
+++ commands.md
@@ -0,0 +1,13 @@
+### Requirements
+
+Installation of:
+
+- [Composer](https://getcomposer.org/) ([https://getcomposer.org/doc/00-intro.md](https://getcomposer.org/doc/00-intro.md))
+- [Laravel](https://laravel.com/) (`composer global require "laravel/installer"`)
+
+
+### Optional Aliases
+
+```
+alias art='php artisan'
+```
---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+// var elixir = require('laravel-elixir');
+// elixir(function(mix) {
+//     mix.sass('app.scss');
+// });
