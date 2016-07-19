c2f24dd6b8a4cb8adf8117f0056d9e85efab4eb2
nok <darius.morawiec@nok.onl>
2016-07-19 10:22:24 +0200

    update profile sub navigation

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,29 +1,41 @@
 @extends('layouts.app')
 
+@section('css')
+.content .actions {
+    border-bottom: 1px solid #eee;
+    padding-top: 4px;
+    padding-bottom: 10px;
+    margin-bottom: 30px;
+}
+@endsection
+
 @section('content')
 <div class="content container">
     <div class="row">
         <div class="col-md-12">
-            <h1>{{ $user->username }}</h1>
+            @if (Auth::check() && Auth::user()->id == $user->id)
+            <div class="actions">
+                <ul class="list-inline">
+                  <li>Actions:</li>
+                  <li>
+                      <a href="{{ route('users.edit', ['id' => $user->id]) }}" class="btn btn-primary btn-sm">Update profile</a>
+                  </li>
+                  <li>
+                      {!! Form::open(array('method' => 'DELETE', 'route' => array('users.destroy', $user->id), 'style' => 'display: inline;')) !!}
+                          {{ Form::submit('Delete account', array('class' => 'btn btn-danger btn-sm')) }}
+                      {!! Form::close() !!}
+                  </li>
+                </ul>
+            </div>
+            @endif
 
+            <h1>{{ $user->username }}</h1>
             @if ($user->name)
               <p>Name: {{ $user->name }}</p>
             @endif
             @if ($user->bio)
               <p>{{ $user->bio }}</p>
             @endif
-
-            @if (Auth::check() && Auth::user()->id == $user->id)
-            <hr>
-            <a href="{{ route('users.edit', ['id' => $user->id]) }}" class="btn btn-primary">Update profile</a>
-
-            {!! Form::open(array('method' => 'DELETE', 'route' => array('users.destroy', $user->id), 'style' => 'display: inline;')) !!}
-                {{ Form::submit('Delete account', array('class' => 'btn btn-danger')) }}
-            {!! Form::close() !!}
-            @endif
-
-            <hr>
-            <a href="{{ route('users.index') }}">List all users</a>
         </div>
     </div>
 </div>
