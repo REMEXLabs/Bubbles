5cd67419f2a01aa60727a715cd092f25e8c5539c
nok <darius.morawiec@nok.onl>
2016-07-26 17:52:09 +0200

    add build and watch process

--- gulpfile.js
+++ gulpfile.js
@@ -15,8 +15,14 @@
 // });
 
 var gulp = require('gulp');
+var sync = require('browser-sync');
+var plug = require('gulp-load-plugins')({
+  rename: {
+    'gulp-connect-php': 'connect'
+  }
+});
 
-var paths = {
+var path = {
   src: {
     sass: 'resources/assets/sass'
   },
@@ -25,4 +31,49 @@ var paths = {
   }
 }
 
-gulp.task('default', function () {});
+gulp.task('sass:build', function () {
+  gulp.src(path.src.sass + '/**/*.scss')
+    .pipe(plug.sass())
+    .pipe(gulp.dest(path.public.css));
+});
+gulp.task('sass:watch', function () {
+  gulp.watch([
+    path.src.sass + '/**/*.scss'
+  ], [
+    'sass:build',
+    'refresh'
+  ]);
+});
+
+gulp.task('connect', function () {
+  plug.connect.server({
+    port: 8000,
+    hostname: '127.0.0.1',
+    base: 'public',
+    keepalive: true,
+    open: false
+  }, function () {
+    sync.init({
+      proxy: '127.0.0.1:8000',
+      port: 3000,
+      notify: false,
+      online: false
+    });
+  });
+});
+gulp.task('php:watch', function () {
+  gulp.watch([
+    'app/**/*.php',
+    'resources/views/**/*.php'
+  ], [
+    'refresh'
+  ]);
+});
+
+gulp.task('refresh', function () {
+  sync.reload();
+});
+
+gulp.task('default', ['build']);;
+gulp.task('build', ['sass:build']);
+gulp.task('watch', ['connect', 'php:watch', 'sass:watch'])
