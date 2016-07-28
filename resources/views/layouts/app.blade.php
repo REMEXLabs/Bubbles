1c6b96e73bbcf45422372b47416f1f969c771aed
nok <darius.morawiec@nok.onl>
2016-07-28 18:13:01 +0200

    add main js file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,5 +78,6 @@
     </footer>
     <script src="/assets/js/jquery.min.js" charset="utf-8"></script>
     <script src="/assets/js/bootstrap.min.js" charset="utf-8"></script>
+    <script src="/assets/js/main.js" charset="utf-8"></script>
 </body>
 </html>
