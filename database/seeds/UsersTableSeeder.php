7d77a7215de33f6f9c0532c67b6558b62227fd7a
nok <darius.morawiec@nok.onl>
2016-08-23 15:25:44 +0200

    extend admin seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -48,7 +48,17 @@ class UsersTableSeeder extends Seeder
                 'role'        => 'admin',
             ),
         );
-        DB::table('users')->insert($users);
+        foreach ($users as $key => $user) {
+            $user = new App\User($user);
+            $user->save();
+            for ($idx=0, $len=mt_rand(1, 4); $idx<$len; $idx++) {
+                $user->projects()->save(factory(App\Project::class)->make());
+            }
+            for ($idx=0, $len=mt_rand(2, 5); $idx<$len; $idx++) {
+                $user->quests()->save(factory(App\Quest::class)->make());
+            }
+        }
+        // DB::table('users')->insert($users);
 
         // Users:
         factory(App\User::class, mt_rand(10, 20))->create()->each(function ($user) {
@@ -56,7 +66,7 @@ class UsersTableSeeder extends Seeder
                 $user->projects()->save(factory(App\Project::class)->make());
             }
             for ($idx=0, $len=mt_rand(2, 5); $idx<$len; $idx++) {
-                        $user->quests()->save(factory(App\Quest::class)->make());
+                $user->quests()->save(factory(App\Quest::class)->make());
             }
         });
     }
