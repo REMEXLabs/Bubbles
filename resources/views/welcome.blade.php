daa52d22623fb9caf7022b8725eb71f6d53dac41
nok <darius.morawiec@nok.onl>
2016-09-20 16:57:08 +0200

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,18 +23,21 @@
     <div class="container">
         <div class="row">
           <div class="col-md-4">
-            <div class="bubble" style="width: {{ $cssRadUsers }}px; height: {{ $cssRadUsers }}px; margin-top: {{ ($cssMarUsers) / 2 }}px; line-height: {{ $cssRadUsers }}px; font-size: {{ $cssFntUsers }}px;">
-              {{ $numUsers }} Users
+            <div class="bubble" style="width: {{ $cssRadUsers }}px; height: {{ $cssRadUsers }}px; margin-top: {{ ($cssMarUsers) / 2 }}px; line-height: {{ $cssRadUsers + 40 }}px; font-size: {{ $cssFntUsers }}px;">
+              <span>{{ $numUsers }} Users</span>
+              <i class="fa fa-user" aria-hidden="true" style="margin-left: -17px; margin-top: -39px;"></i>
             </div>
           </div>
           <div class="col-md-4">
-            <div class="bubble" style="width: {{ $cssRadQuests }}px; height: {{ $cssRadQuests }}px; margin-top: {{ ($cssMarQuests) / 2 }}px; line-height: {{ $cssRadQuests }}px; font-size: {{ $cssFntQuests }}px;">
-              {{ $numQuests }} Quests
+            <div class="bubble" style="width: {{ $cssRadQuests }}px; height: {{ $cssRadQuests }}px; margin-top: {{ ($cssMarQuests) / 2 }}px; line-height: {{ $cssRadQuests + 40 }}px; font-size: {{ $cssFntQuests }}px;">
+              <span>{{ $numQuests }} Quests</span>
+              <i class="fa fa-bolt" aria-hidden="true" style="margin-left: -11px; margin-top: -40px;"></i>
             </div>
           </div>
           <div class="col-md-4">
-            <div class="bubble" style="width: {{ $cssRadProjects }}px; height: {{ $cssRadProjects }}px; margin-top: {{ ($cssMarProjects) / 2 }}px; line-height: {{ $cssRadProjects }}px; font-size: {{ $cssFntProjects }}px;">
-              {{ $numProjects }} Projects
+            <div class="bubble" style="width: {{ $cssRadProjects }}px; height: {{ $cssRadProjects }}px; margin-top: {{ ($cssMarProjects) / 2 }}px; line-height: {{ $cssRadProjects + 40 }}px; font-size: {{ $cssFntProjects }}px;">
+              <span>{{ $numProjects }} Projects</span>
+              <i class="fa fa-code" aria-hidden="true" style="margin-left: -23px; margin-top: -39px;"></i>
             </div>
           </div>
         </div>
