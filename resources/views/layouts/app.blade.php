e8972e0b81f480064873f3d083ed126ef2dc9a27
Sebastian Koch <sebastian@0py.de>
2018-07-06 13:23:27 +0200

    Updates layout file to include cookie consent banner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,5 +110,7 @@
       </div>
     </footer>
     <script src="/assets/js/main.js" charset="utf-8"></script>
+    <script src="/assets/js/helper.js" charset="utf-8"></script>
+    <script>remex_cookies_wp_language = 'en';</script>
 </body>
 </html>
